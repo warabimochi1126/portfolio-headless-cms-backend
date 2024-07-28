69cf7561a082adbabf2332c833791433b7b95062
warabimochi <satomi920003@yahoo.co.jp>
2024-07-28 19:10:53 +0900

    feat:#4 NestJSとNeonの疎通確認を行う

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from 'typeOrmConfig';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
