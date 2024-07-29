3a901458d759a85971ec385d82c821754fb30a04
warabimochi <ecccompsenyou@gmail.com>
2024-07-29 14:54:01 +0900

    feat:#10 Entityのカラムにnull許容を追加する

--- src/entity/product.entity.ts
+++ src/entity/product.entity.ts
@@ -1,28 +1,28 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    imageSrcPath: string;
+  @Column({ nullable: true })
+  imageSrcPath: string | null;
 
-    @Column()
-    deployUrl: string;
+  @Column({ nullable: true })
+  deployUrl: string | null;
 
-    @Column()
-    productName: string;
+  @Column()
+  productName: string;
 
-    @Column()
-    overview: string;
+  @Column()
+  overview: string;
 
-    @Column()
-    mainTechnology: string;
+  @Column()
+  mainTechnology: string;
 
-    @Column()
-    subTechnology: string;
+  @Column({ nullable: true })
+  subTechnology: string | null;
 
-    @Column("text", { array: true })
-    productLinks: string[];
+  @Column('text', { array: true, nullable: true })
+  productLinks: string[] | null;
 }
