5b272ac0c63a612b098498b3cb3c7c2731b1a739
warabimochi <ecccompsenyou@gmail.com>
2024-07-30 13:22:26 +0900

    feat:#13 エンドポイントを作成してcreate機能を実装する

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from 'src/config/typeormConfig';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig), ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
